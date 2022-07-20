635183506bab740de339fa9555a4abe1ffe0f1d6
Enio Schaefer <enio100schaefer@hotmail.com>
2022-07-20 19:12:46 -0300

    exercicios conteudos bloco 04 / dia 01 pt03 pt04 e pt05

---
+++ fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_javascript_primeiros_passos/conteudos/pt03/scripts.js
@@ -0,0 +1,9 @@
+const testGrade = 85;
+
+if (testGrade >= 80) {
+    console.log("Parabens, voce foi aprovada(o)!");
+} else if (testGrade < 80 && testGrade >= 60) {
+    console.log("Voce esta na nossa lista de espera");
+} else {
+    console.log("Voce foi reprovada(o)");
+}
---
+++ fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_javascript_primeiros_passos/conteudos/pt04/scripts.js
@@ -0,0 +1,33 @@
+// const currentHour = 16;
+// let message = "";
+
+// if (currentHour >= 22) {
+//     message = "Nao deveriamos comer nada, e hora de dormir";
+// } else if (currentHour >= 18 && currentHour < 22) {
+//     message = "Rango da noite, vamos jantar :D";
+// } else if (currentHour >= 14 && currentHour < 18) {
+//     message = "Vamos fazer um bolo pro cafe da tarde?";
+// } else if (currentHour >=11 && currentHour <= 14) {
+//     message = "Hora do almoco!!!";
+// } else if (currentHour >= 4 && currentHour <= 11) {
+//     message = "Hmmm, cheiro de cafe recem passado";
+// }
+
+// console.log(message);
+
+// ------------------------------------------------------------------------------------
+
+// let weekDay = "quarta-feira";
+
+// if (weekDay == "segunda-feira" || weekDay == "terca-feira" || 
+// weekDay == "quarta-feira" || weekDay == "quinta-feira" ||
+// weekDay == "sexta-feira") {
+//     console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+// } else if (weekDay == "domingo" || weekDay == "sabado") {
+//     console.log("FINALMENTE, descanso merecido UwU");
+// }
+
+// -----------------------------------------------------------------------------------
+
+// operador NOT = um boolean ao contrario
+//console.log(!(2 + 2) === 4);
