c6bbe46f442999900ece5b7e9b1a8091674fdba1
Enio Schaefer <enio100schaefer@hotmail.com>
2022-07-27 18:28:03 -0300

    exercicios conteudo pt01 e pt02

---
+++ fundamentos/bloco_05_javascript_dom_eventos_e_web_storage/dia_01_dom_e_seletores/conteudos/pt01/scripts.js
@@ -0,0 +1,6 @@
+document.getElementById('page-title').innerText = 'Interstellar';
+document.getElementById('second-paragraph').innerText = 'Paragrafo que vem depois do primeiro e antes do terceiro';
+document.getElementById('second-paragraph').style.color = 'green';
+document.getElementById('subtitle').innerText = 'luiz molis, sem aquela parte.';
+document.getElementsByClassName('parag')[0].style.color = 'purple';
+document.getElementsByTagName('h4')[0].style.color = 'yellow';
---
+++ fundamentos/bloco_05_javascript_dom_eventos_e_web_storage/dia_01_dom_e_seletores/conteudos/pt02/script.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = '#FFA085ff';
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = '#F9DC5Eff';
+document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = '#A600F3ff';
+document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = '#A600F3ff';
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor = '#1E2020ff';
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = '#1E2020ff';
+document.getElementById('container').style.backgroundColor = '#F3F3F3ff';
+document.getElementById('footer-container').style.backgroundColor = '#003230ff';
+document.getElementById('header-container').style.backgroundColor = '#00B269ff';
