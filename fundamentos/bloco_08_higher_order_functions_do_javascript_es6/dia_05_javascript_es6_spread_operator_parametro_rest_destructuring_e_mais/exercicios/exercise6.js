e9de25ac78a7b381bfdf5e6f67e052b7d4468364
Enio Schaefer <enio100schaefer@hotmail.com>
2022-08-25 20:44:08 -0300

    exercicios completos

---
+++ fundamentos/bloco_08_higher_order_functions_do_javascript_es6/dia_05_javascript_es6_spread_operator_parametro_rest_destructuring_e_mais/exercicios/exercise6.js
@@ -0,0 +1,16 @@
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+// escreva toObject abaixo
+
+const toObject = (car) => {
+const [model, brand, year] = car;
+  return carObject = {
+    model,
+    brand,
+    year, 
+  };
+};
+
+console.log(toObject(palio));
